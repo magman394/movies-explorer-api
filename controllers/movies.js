5b3c75b2a1a2ebedf4353d44952719400f0b450a
magman394 <ibelokopytov@gmail.com>
2021-11-30 01:22:07 +0300

    fix: Баги и ошибки по Ревью 2

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,14 +9,13 @@ module.exports.getmovies = (req, res, next) => {
 
 module.exports.deletemovie = (req, res, next) => {
   const { id } = req.params;
-  Movie.findById(id)
+  Movie.findOne({ movieId: id })
     .orFail(new NotFoundError('Такого фильма нет в избранном'))
     .then((movie) => {
       if (movie.owner.toString() !== req.user._id) {
         throw new ForbiddenError('Данного фильма нет в избранном. Удалить её нельзя');
       } else {
-        res.send({ message: 'Фильм удален из избранного' });
-        movie.remove().then(() => res.send({ message: movie }));
+        movie.remove().then(() => res.send(movie));
       }
     }).catch(next);
 };
